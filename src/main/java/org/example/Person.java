d1048ff812a6bdae35a9a0bebe77172d990635dd
Alex <alexfusun@gmail.com>
2023-02-14 12:38:50 +0100

    Added Person class

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.List;
+
+public class Person {
+
+    private final String name;
+    private final int age;
+    private final String gender;
+
+    public Person(String name, int age, String gender) {
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public int age() {
+        return age;
+    }
+
+    public String gender() {
+        return gender;
+    }
+
+    /**
+     * Computes the average age of male and female persons in a list and returns the result in
+     * an array of two elements( the first element is the male mean age and the second one is the
+     * female mean age)
+     *
+     * @param persons
+     * @return
+     */
+    public double[] averageAgePerGender(List<Person> persons) {
+
+    }
+}
